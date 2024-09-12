15ed1cdb34f7bf707be095c90a0c8bc370a98201
mantrazul <blue@blue.>
2024-09-12 18:51:05 -0300

    add: player function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 COLOR_SCREEN = (0, 0, 0)
 
@@ -10,6 +11,7 @@ def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -19,8 +21,10 @@ def main():
             
         pygame.Surface.fill(screen, COLOR_SCREEN)
         pygame.display.flip()
-
+        player.draw(screen)
         dt = clock.tick(60) / 1000
+
+
 
 
 if __name__ == "__main__":
