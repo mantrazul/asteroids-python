1eeb130a1dbb0ae2a3c1d5e27a7fdc3e0c801ceb
mantrazul <blue@blue.>
2024-09-14 12:43:41 -0300

    add: collision

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
 from player import Player
@@ -33,6 +34,12 @@ def main():
 
         for obj in updatable:
             obj.update(dt)
+
+
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game Over!")
+                sys.exit()
 
         screen.fill("black")
 
