37e5e01b83c935779a02f54074c9efcf0e1dabfd
mantrazul <blue@blue.>
2024-09-14 13:40:21 -0300

    add: asteroid splits now

--- asteroid.py
+++ asteroid.py
@@ -13,3 +13,19 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        random_angle = random.uniform(20, 30)
+        first = self.velocity.rotate(random_angle)
+        second = self.velocity.rotate(-random_angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        new_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid.velocity = first * 1.2
+        new_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid.velocity = second * 1.2
--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def main():
             for shot in shots:
                 if asteroid.collides_with(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         screen.fill("black")
         for obj in drawable:
